cd34a0b08d161eacf1d04325f4bbef4c139db2fc
Dev Say <game@devsay.com>
2018-10-01 19:15:58 -0300

    spanish layout.php added

---
+++ src/es/layout.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'cookie' => [
+        'button' => 'Ok, muestrame juegos de mesa!',
+        'text' => 'Este sitio web usa cookies para recordar tus preferencias. De esta manera podemos modificar el contenido y mostrarte lo que más te interesa '
+    ],
+
+    'footer' => [
+        'text' => '**TableTopFinder** es un proyecto creado por Jeroen Deviaene. Este sitio te da la posibilidad de encontrar juegos de mesa de tu agrado tan solo con buscar algunas palabras clave. Y luego listaremos los mejores precios europeos online de dichos juegos'
+    ],
+
+    'follow' => [
+        'facebook' => 'Síguenos en Facebook',
+        'instagram' => 'Síguenos en Instagram',
+        'twitter' => 'Síguenos en Twitter'
+    ]
+];
+
+is a project created by Jeroen Deviaene. This website gives you the possibility to find board games that you will like just by searching on a few keywords. And after that, we list the best European prices for these board games online.
