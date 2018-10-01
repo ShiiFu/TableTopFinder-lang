62f3347f9caa6e82043eef4172a9b444860f822e
Dev Say <game@devsay.com>
2018-10-01 17:48:01 -0300

    spanish keywords.php added

---
+++ src/es/keywords.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ *
+ *  These are translated keywords used for alt tags for images
+ *
+ */
+
+return [
+
+    'animals' => 'animales',
+    'board' => 'tablero',
+    'boardgames' => 'juegos de mesa',
+    'box' => 'caja',
+    'boxback' => 'detrás de la caja',
+    'card' => 'carta',
+    'cards' => 'cartas',
+    'characters' => 'personajes',
+    'coins' => 'monedas',
+    'collection' => 'colección',
+    'cubes' => 'cubos',
+    'components' => 'componentes',
+    'dice' => 'dados',
+    'dragon' => 'dragón',
+    'dragons' => 'dragones',
+    'gameboard' => 'tablero de juego',
+    'gameplay' => 'gameplay',
+    'gems' => 'gemas',
+    'horses' => 'caballos',
+    'locations' => 'lugares',
+    'logo' => 'logo',
+    'manual' => 'manual',
+    'miniature' => 'miniatura',
+    'miniatures' => 'miniaturas',
+    'money' => 'dinero',
+    'pawns' => 'peones',
+    'planet' => 'planeta',
+    'planets' => 'planetas',
+    'tiles' => 'baldosas',
+
+];
