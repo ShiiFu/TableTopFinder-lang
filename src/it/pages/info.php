75340a08dc2d8620580c6eb654e522762fffb997
Andriy Frankevych <andriy.frankevych@gmail.com>
2018-10-02 12:54:06 +0200

    Fix info.php

--- src/it/pages/info.php
+++ src/it/pages/info.php
@@ -6,17 +6,17 @@ return [
         'successmessage' => 'Il tuo messaggio è stato inviato! Ti contatteremo al più presto.'
     ],
     'disclaimer' => [
-        'body' => 'Il proprietario del sito web i suoi fornitori di informazioni non possono essere ritenuti responsabili per eventuali interruzioni o informazioni contenuti sul sito.
+        'body' => "Il proprietario del sito web i suoi fornitori di informazioni non possono essere ritenuti responsabili per eventuali interruzioni o informazioni contenuti sul sito.
 Le informazioni fornite si basano su fonti affidabili e un'accurata analisi. Il proprietario del sito non può essere ritenuto responsabile per l'accuratezza, la completezza o l'adeguatezza delle informazioni per qualsiasi scopo.
 Il proprietario del sito web non è responsabile per qualsiasi decisione o azione presa sulla base delle informazioni fornite da questo sito. Il proprietario del sito non è responsabile per eventuali errori o omissioni e non è responsabile nei confronti dell'utente o di terzi per qualsiasi danno diretto, indiretto, incidentale, perdita di profitti, perdita di opportunità o qualsiasi altro danno causato dalla sua negligenza o omissioni nella raccolta, compilazione, scrittura, interpretazione, segnalando e distribuendo informazioni o dati attraverso questo servizio, anche se il proprietario del sito web ha avvertito di tale danno.
-Il sito contiene molti collegamenti ad altri siti web, questi collegamenti sono puramente informativi e il proprietario del sito non è responsabile per il contenuto o l'uso che potrebbe esserne fatto.'
+Il sito contiene molti collegamenti ad altri siti web, questi collegamenti sono puramente informativi e il proprietario del sito non è responsabile per il contenuto o l'uso che potrebbe esserne fatto."
     ],
     'faq' => [
         'title' => 'Domande frequenti',
         'categories' => [
             'Generali' => [
-                'Quando vengono aggiornati i prezzi del gioco da tavolo?' => 'I nostri server aggiornano costantemente i prezzi sul sito web. Tuttavia, a causa dell'enorme quantità di negozi e giochi da tavolo che devono essere aggiornati, possono essere necessarie fino a 72 ore prima che venga registrato un aggiornamento dei prezzi.',
-                'Posso aggiungere o richiedere l'aggiunta di un gioco da tavolo al sito?' => 'Puoi chiedere di aggiungere un gioco da tavolo sul sito web se non è ancora lì. Puoi usare [il form di contatto](info.contact) per farci sapere il gioco che manca.',
+                'Quando vengono aggiornati i prezzi del gioco da tavolo?' => "I nostri server aggiornano costantemente i prezzi sul sito web. Tuttavia, a causa dell'enorme quantità di negozi e giochi da tavolo che devono essere aggiornati, possono essere necessarie fino a 72 ore prima che venga registrato un aggiornamento dei prezzi.",
+                "Posso aggiungere o richiedere l'aggiunta di un gioco da tavolo al sito?" => 'Puoi chiedere di aggiungere un gioco da tavolo sul sito web se non è ancora lì. Puoi usare [il form di contatto](info.contact) per farci sapere il gioco che manca.',
                 'Potete fare pubblicità il mio sito/negozio/gioco da tavolo su questo sito?' => 'Siamo aperti alle vostre richieste di pubblicità. Puoi contattarci per ulteriori informazioni attraverso [il form di contatto](info.contact).'
             ],
             'Editori di giochi da tavolo' => [
