dc04bdca17cb7bb3b272e3763d85e56d2fb9f73b
Andriy Frankevych <andriy.frankevych@gmail.com>
2018-10-02 12:10:57 +0200

    Create layout.php

---
+++ src/it/layout.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'cookie' => [
+        'button' => 'Ok, cerchiamo dei giochi da tavolo!',
+        'text' => 'Questo sito utilizza i cookies per ricordare le tue preferenze. In questo modo possiamo offrirti il contenuto più addatto a te.'
+    ],
+    'footer' => [
+        'text' => '**TableTopFinder** è un progetto creato da Jeroen Deviaene. Questo sito offre la posibilità di trovare giochi da tavolo che ti piaceranno con una semplice ricerca per qualche parola chive. E dopo questo, ti mostreremo i prezzi migliori in Europa per acquistare questi giochi online.'
+    ],
+    'follow' => [
+        'facebook' => 'Suguici su Facebook',
+        'instagram' => 'Suguici su Instagram',
+        'twitter' => 'Suguici su Twitter'
+    ]
+];
