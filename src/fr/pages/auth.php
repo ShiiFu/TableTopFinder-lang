6481a6d9ef46c40d44571413910d45b746b212e1
nbrabant <brabantnicolas59@gmail.com>
2018-10-09 22:45:54 +0200

    Add missing and fix french i18n

--- src/fr/pages/auth.php
+++ src/fr/pages/auth.php
@@ -7,38 +7,38 @@ return [
         'accountActivated' => '**Votre compte est maintenant activé!**
 Vous pouvez maintenant vous connecter et profiter de toutes les fonctionnalités du site.',
         'accountCreated' => '**Votre compte a été créé!**
-Dans quelques instants, vous recevrez un e-mail avec le lien d\'activation pour votre compte.',
+Dans quelques instants, vous recevrez un e-mail avec le lien d\'activation de votre compte.',
         'accountNotActivated' => 'Votre compte n\'est pas encore activé.
- Vérifiez votre boîte de courriels pour votre lien d\'activation.',
+Vérifiez votre boîte de réception afin d\'activer votre compte.',
         'addedToList' => 'Le jeu a été ajouté à la liste [:list](:url).',
         'addToGameList' => 'Ajouter à une liste des jeux',
         'createAccount' => 'Créer un compte',
-        'forgotPassword' => 'mot de passe oublié',
-        'forgotPasswordInfo' => 'Entrez votre adresse e-mail ici et nous vous enverrons un lien avec lequel vous pourrez réinitialiser votre mot de passe.',
+        'forgotPassword' => 'Mot de passe oublié',
+        'forgotPasswordInfo' => 'Saisissez votre adresse e-mail, nous vous enverrons un lien avec lequel vous pourrez réinitialiser votre mot de passe.',
         'invalidCredentials' => 'Données non valides',
         'passwordIsReset' => '**Votre mot de passe a été réinitialisé**
-Vous pouvez maintenant vous connecter avec le nouveau mot de passe.',
+Vous pouvez maintenant vous connecter avec votre nouveau mot de passe.',
         'passwordReset' => 'Réinitialiser le mot de passe',
-        'passwordResetInfo' => 'Entrez votre nouveau mot de passe pour votre compte ici. Après avoir envoyé ce formulaire, vous pourrez vous connecter avec ce nouveau mot de passe.',
+        'passwordResetInfo' => 'Entrez le nouveau mot de passe pour votre compte. Après avoir envoyé ce formulaire, vous pourrez vous connecter avec ce nouveau mot de passe.',
         'passwordResetSent' => '**succès!**
-Un lien est envoyé immédiatement pour réinitialiser votre mot de passe.',
+Un lien est envoyé immédiatement afin de réinitialiser votre mot de passe.',
         'profilesaved' => 'Votre profil a été enregistré',
         'settingsSaved' => 'Vos paramètres ont été enregistrés',
         'subscriptionCreated' => 'L\'abonnement a été ajouté avec succès
  Cela peut toujours être ajusté dans votre profil.',
-        'subscriptionInfo' => 'Lorsque le prix de ce jeu est inférieur à une certaine valeur, vous recevrez une notification.',
+        'subscriptionInfo' => 'Vous recevrez une notification lorsque le prix de ce jeu sera inférieur à une certaine valeur.',
         'subscriptionRemove' => 'L\'abonnement a été supprimé',
         'subscriptionUpdate' => 'L\'abonnement a été ajusté'
     ],
 
     'registerfeatures' => [
-        'events' => 'Trouvez des événements de jeux de société et des conférences autour de vous',
+        'events' => 'Trouvez des événements et des conférences de jeux de société à proximité',
         'lists' => 'Créer des listes de jeux à partager avec des amis',
-        'more' => 'Plus de fonctionnalités approchent',
-        'notifications' => 'Recevoir des notifications lorsqu\'un jeu tombe en dessous d\'un certain prix'
+        'more' => 'Fonctionnalité à venir',
+        'notifications' => 'Recevoir une notification lorsqu\'un jeu tombe en dessous d\'un certain prix'
     ],
 
-    'accountinfo' => 'informations de compte',
+    'accountinfo' => 'mes informations',
     'emailnotifications' => 'notifications par e-mail',
     'gamegroups' => 'groupes de jeux',
     'lists' => 'listes de jeux',
