0704a669f97ed82a0fed0dde7e6c80767c79d29d
nbrabant <brabantnicolas59@gmail.com>
2018-10-09 22:48:53 +0200

    +1

---
+++ src/fr/email.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'accountactivation' => [
+        'preview' => 'Votre inscription sur TableTopFinder est terminée, voici votre lien d\'activation de votre compte',
+        'intro' => 'Bonjour :displayname,
+
+Votre inscription sur TableTopFinder est maintenant terminée, il ne vous reste plus qu\'à cliquer sur sur le lien suivant afin d\'activer votre compte.',
+        'footer' => 'Profitez bien du site!
+L\'équipe de TableTopFinder',
+        'button' => 'Activer mon compte',
+        'subject' => 'Activer mon compte sur TableTopFinder'
+    ],
+    'passwordforget' => [
+        'preview' => 'Votre demande de réinitialisation de mot de passe a été envoyé',
+        'intro' => 'Bonjour :displayname,
+
+Une réinitialisation du mot de passe de votre compte [TableTopFinder](https://www.tabletopfinder.eu/) a été demandée. Vous pourrez trouver ce lien ci-dessous.
+Si vous n\'êtes pas à l\'origine de cette demande, vous pouvez ignorer cet email.',
+        'button' => 'Réinitialiser mon mot de passe',
+    ]
+];
---
+++ src/fr/pages/info.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+    'contact' => [
+        'title' => 'Contact',
+        'description' => 'TableTopFinder est un site web où les visiteurs peuvent trouver de nouveaux jeux de plateau ou des actualités sur les jeux de plateau. Vous êtes libre de nous contacter si votre jeu de plateau favori n\'est pas présent sur le site ou si vous souhaitez écrire quelque chose à propos de votre propre jeu de plateau.',
+        'successmessage' => 'Votre message a été envoyé! Nous vous contacterons prochainement.'
+    ],
+
+    'disclaimer' => [
+        'body' => 'Le propriétaire du site ou ses fournisseurs d\'informations ne peuvent être tenus pour responsable des perturbations ou des informations présentes sur le site.
+
+Les informations fournies sont basées sur des sources fiables et une analyse minutieuse. Le propriétaire du site décline toute responsabilité en cas d\'erreur, d\'inexactitude ou d\'omission affectant les informations
+
+Le propriétaire du site ne peut être tenus pour responsable pour les décisions ou actions faite depuis la base des informations fournies sur le site. Il n\'est pas non plus responsable d\'erreurs ou d\'omissions, ni envers l\'utilisateur ou des parties tierces pour tout dommage direct, indirect, accidentel, manque à gagner, perte d\'opportunité ou tout autre dommage causé par sa négligeance ou omission lors de la collecte, la compilation, l\'écriture, l\'interpretation, le report et la distribution de l\'information ou de la donnée depuis ce service, et ce, même si le propriétaire du site l\'avertissait de ces dommages.
+
+Le site contient des liens vers d\'autres sites internet, ceux-ci sont purement informatifs et le propriétaire du site ne peut pas être tenu responsable pour le contenu ou l\'utilisation qui peut en être fait.'
+    ],
+
+    'faq' => [
+        'title' => 'Questions fréquement posées',
+        'categories' => [
+            'Général' => [
+                'A quel moment les prix de jeux de plateau sont mis à jour?' => 'Nos serveurs mettent à jour constamment les prix sur le site. Toutefois, en raison du nombre considérable de magasins et de jeux de société nécessitant une mise à jour, l\'enregistrement d\'une mise à jour de prix peut prendre jusqu\'à 72 heures.',
+                'Puis-je ajouter ou demander qu\'un jeu de plateau soit ajouté sur le site?' => 'Vous pouvez bien entendu demander l\'ajout d\'un jeu de plateau sur le site s\'il n\'y est pas encore. Vous êtes libre d\'utiliser [le formulaire de contact](info.contact) afin de nous communiquer le jeu est manquant.',
+
+                'Pouvez-vous annoncer mon site / magasin / jeu de société sur votre site?' => 'Nous sommes ouverts à vos demande de publicité. Vous pouvez nous contacter pour plus d\'information via [le formulaire de contact](info.contact).'
+            ],
+            'éditeurs de jeux de plateau' => [
+                'Pouvez-vous ajouter une critique de notre jeu sur votre site?' => 'Bien entendu, si vous pouver nous envoyer une copie ou un prototype, nous pourrons tester le jeu et nous seront heureux d\'écrire un bonne critique de celui-ci.'
+            ],
+            'Propriétaires de magasins' => [
+                'Pouvez-vous ajouter notre magasin sur votre site?' => 'Bien entendu, nous souhaitons que notre site contienne le plus d\'informations possible. Veuillez utiliser [le formulaire de contact](info.contact) afin de nous envoyer votre boutique en ligne et les informations dont nous avons besoin.'
+            ]
+        ]
+    ],
+
+    'links' => [
+        'title' => 'Liens de jeu de plateau',
+
+        'categories' => [
+            'associations' => 'Associations',
+            'communities' => 'Communautés',
+        ],
+
+        'descriptions' => [
+            'boardgamegeek' => 'BoardGameGeek est la plus grande communauté de jeux de plateau du monde. Elle a été créée en 2000 par Scott Alden et Derk Solko.'
+        ]
+    ]
+
+];
